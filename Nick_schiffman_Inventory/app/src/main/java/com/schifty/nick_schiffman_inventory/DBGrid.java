331be76b298a021a396db8a5c2a01866deffdf6f
Nick Schiffman <>
2023-02-14 19:20:52 -0500

    DB

--- Nick_schiffman_Inventory/app/src/main/java/com/schifty/nick_schiffman_inventory/DBGrid.java
+++ Nick_schiffman_Inventory/app/src/main/java/com/schifty/nick_schiffman_inventory/DBGrid.java
@@ -1,11 +1,13 @@
 package com.schifty.nick_schiffman_inventory;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 
@@ -17,6 +19,24 @@ public class DBGrid extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_dbgrid);
+
+
+        public void getMoviesByRating(String rating) {
+            SQLiteDatabase db = getReadableDatabase();
+
+            String sql = "select * from " + MovieTable.TABLE + " where rating = ?";
+            Cursor cursor = db.rawQuery(sql, new String[] { rating });
+            if (cursor.moveToFirst()) {
+                do {
+                    long id = cursor.getLong(0);
+                    String title = cursor.getString(1);
+                    String rated = cursor.getString(2);
+                    float stars = cursor.getFloat(3);
+
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+        }
 
     }
 
