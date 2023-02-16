1e702581a7a0f9770ad261cb459a6061b8fe9863
Nick Schiffman <>
2023-02-15 21:10:44 -0500

    Creating model

---
+++ Nick_schiffman_Inventory/app/src/main/java/com/schifty/nick_schiffman_inventory/InventoryModel.java
@@ -0,0 +1,52 @@
+package com.schifty.nick_schiffman_inventory;
+
+public class InventoryModel {
+
+    private int id;
+    private String item;
+    private int inventory;
+    private String discription;
+
+
+    public InventoryModel(int id, String item, int inventory, String discription){
+        this.id = id;
+        this.item = item;
+        this.inventory = inventory;
+        this.discription = discription;
+    }
+    public InventoryModel(){
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+}
--- Nick_schiffman_Inventory/app/src/main/java/com/schifty/nick_schiffman_inventory/db.java
+++ Nick_schiffman_Inventory/app/src/main/java/com/schifty/nick_schiffman_inventory/db.java
@@ -1,37 +1,37 @@
-package com.schifty.nick_schiffman_inventory;
-
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-
-public class InventoryDb extends SQLiteOpenHelper {
-
-    private static final String DATABASE_NAME = "movies.db";
-    private static final int VERSION = 1;
-
-    public InventoryDb(Context context) {
-        super(context, DATABASE_NAME, null, VERSION);
-    }
-
-    private static final class InventoryTable {
-        private static final String TABLE = "TABLE";
-        private static final String item = "item";
-        private static final String description = "description";
-        private static final String inventory = "inventory";
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + InventoryTable.TABLE + " (" +
-                InventoryTable.item + " integer primary key autoincrement, " +
-                InventoryTable.description + " text, " +
-                InventoryTable.inventory + " text, )");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion,
-                          int newVersion) {
-        db.execSQL("drop table if exists " + InventoryTable.TABLE);
-        onCreate(db);
-    }
-}
+//package com.schifty.nick_schiffman_inventory;
+//
+//import android.content.Context;
+//import android.database.sqlite.SQLiteDatabase;
+//import android.database.sqlite.SQLiteOpenHelper;
+//
+//public class InventoryDb extends SQLiteOpenHelper {
+//
+//    private static final String DATABASE_NAME = "movies.db";
+//    private static final int VERSION = 1;
+//
+//    public InventoryDb(Context context) {
+//        super(context, DATABASE_NAME, null, VERSION);
+//    }
+//
+//    private static final class InventoryTable {
+//        private static final String TABLE = "TABLE";
+//        private static final String item = "item";
+//        private static final String description = "description";
+//        private static final String inventory = "inventory";
+//    }
+//
+//    @Override
+//    public void onCreate(SQLiteDatabase db) {
+//        db.execSQL("create table " + InventoryTable.TABLE + " (" +
+//                InventoryTable.item + " integer primary key autoincrement, " +
+//                InventoryTable.description + " text, " +
+//                InventoryTable.inventory + " text, )");
+//    }
+//
+//    @Override
+//    public void onUpgrade(SQLiteDatabase db, int oldVersion,
+//                          int newVersion) {
+//        db.execSQL("drop table if exists " + InventoryTable.TABLE);
+//        onCreate(db);
+//    }
+//}
